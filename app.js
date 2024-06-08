77ab348d5c9dcb42ded613f773fc70c219c073f3
suresh2648 <152902094+suresh2648@users.noreply.github.com>
2024-06-08 05:52:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+let area=document.getElementById("area")
+let label=document.getElementById("lab")
+let parameter=document.getElementById("parameter")
+let label1=document.getElementById("lab1")
+
+let rarea=document.getElementById("rarea")
+let rlabel=document.getElementById("rlab")
+let rparameter=document.getElementById("rparameter")
+let rlabel1=document.getElementById("rlab1")
+
+area.onclick=function(){
+label.textContent=`the area of the circle is *PI*radius*radius`
+
+}
+
+parameter.onclick=function(){
+    label1.textContent=`the parameter of the circle is 2*PI*radius`
+    }
+
+    rarea.onclick=function(){
+        rlabel.textContent=`the area of the rectangle is *PI*radius*radius `
+        }
+        
+        rparameter.onclick=function(){
+            rlabel1.textContent=`the parameter of the rectangle is 2*(length+berth)`
+            }
+        
+
+            function add(){
+                
+            }
